417cc65210cd4b499214d32c3d2623399c470c57
Wendel Lopes <wendelopes@msn.com>
2021-10-28 09:28:14 -0300

    Created note form and new/edit page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,16 +1,48 @@
 class NotesController < ApplicationController
 
-  before_action :set_note, only: %i[ show ]
-  before_action :set_search, only: %i[ index show ]
+  before_action :set_note, only: %i[ show edit update destroy ]
+  before_action :set_search, except: %i[ update destroy ]
 
   def index; end
 
   def show; end
 
+  def edit; end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(params_note)
+    if @note.save
+      redirect_to @note
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @note.update(params_note)
+      redirect_to @note
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to root_path
+  end
+
   private
 
   def set_note
     @note = Note.find(params[:id])
+  end
+
+  def params_note
+    params.require(:note).permit(:title, :content, :published, :priority)
   end
 
   def set_search
